de17c5a3489eaa8abb610a6bf30064a631888bd6
ARTIST12 <wssu77@gmail>
2025-02-10 08:00:13 +0800

    update 2 button

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, abort, jsonify
+from flask import Flask, request, abort, jsonify, redirect
 from linebot.v3 import WebhookHandler
 from linebot.v3.exceptions import InvalidSignatureError
 from linebot.v3.webhooks import (
